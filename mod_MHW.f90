f1cd8530441bb74ae9a323fb6ca867255ce1921f
blackcata <blackcata109@gmail.com>
2019-02-21 03:57:12 +0900

    Add details to MHW setup subroutine

--- mod_MHW.f90
+++ mod_MHW.f90
@@ -13,7 +13,7 @@
 
             IMPLICIT NONE
 
-            INTEGER  :: window 
+            INTEGER  :: window, Nt_yr, N_percent
             REAL(KIND=8)  :: percent
 
             REAL(KIND=8),ALLOCATABLE,DIMENSION(:)      ::  lat, lon, time  
@@ -33,16 +33,35 @@
 !                                                             2019.02.21.K.Noh !
 !                                                                              !
 !------------------------------------------------------------------------------!
-          SUBROUTINE MHW_setup(Nx,Ny)
+          SUBROUTINE MHW_setup(Nx,Ny,Nt)
 
               IMPLICIT NONE            
-              INTEGER, INTENT(IN)  ::  Nx, Ny
+              INTEGER, INTENT(IN)  ::  Nx, Ny, Nt
+
+              window     =  5
+              percent    =  90.0
+              Nt_yr      =  Nt / 365
+              N_percent  =  INT( (2*window+1)*(Nt_yr-2)*(percent/100.0) )
+
               ALLOCATE( sst_clim(1:Nx,1:Ny,1:365) ) 
               ALLOCATE( sst_percentile(1:Nx,1:Ny,1:365) )
-
-              window   =  5
-              percent  = 90.0
-
+              ALLOCATE( sst_tmp(1:Nx,1:Ny,1:(2*window+1)*(Nt_yr-2)) )
+              
           END SUBROUTINE MHW_setup
+
+!------------------------------------------------------------------------------!
+!                                                                              !
+!   SUBROUTINE : MHW_clim                                                      !
+!                                                                              !
+!   PURPOSE : Calculate the specific range window climatologic mean            !
+!                                                                              !
+!                                                             2019.02.21.K.Noh !
+!                                                                              !
+!------------------------------------------------------------------------------!
+          SUBROUTINE MHW_clim
+
+              IMPLICIT NONE            
+
+          END SUBROUTINE MHW_clim
 
         END MODULE mod_MHWs
